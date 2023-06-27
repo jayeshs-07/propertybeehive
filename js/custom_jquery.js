81fb9c3c6892a3eb1d8625467a62813a2889af79
parthikhm <parhikmodi43@gmil.com>
2023-06-27 12:12:56 +0530

    new project

---
+++ js/custom_jquery.js
@@ -0,0 +1,8 @@
+// $('.utf-more-search-options-area-button').on('click', function (e) {
+//     e.preventDefault();
+//     $('.utf-more-search-options-area, .utf-utf-more-search-options-area-button').toggleClass('active');
+//     $('.utf-more-search-options-area.relative').animate({
+//         height: 'toggle',
+//         opacity: 'toggle'
+//     }, 300);
+// });
